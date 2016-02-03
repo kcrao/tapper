675bf91d97c7b2f6e06a377e9ae41834690c5a8f
kcrao <kcrao@hotmail.com>
2016-02-03 17:30:11 -0600

    Initial Commit

---
+++ udemy app - tapper-extreme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  udemy app - tapper-extreme
+//
+//  Created by Kishore Rao on 2/3/16.
+//  Copyright © 2016 informed-planet.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
